c7caf35659645323013c406dd18faf0e2f23bce1
Nelson Lamounier <lamounierleao@gmail.com>
2026-05-13 10:45:18 +0100

    chore(ingestion): add jest.config.js to fix test runner resolution

--- applications/jest.config.js
+++ applications/jest.config.js
@@ -1,11 +1,23 @@
 /** @format */
 
-const { cjsConfig } = require('../jest.config.base.cjs');
-
 module.exports = {
-  ...cjsConfig,
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
   testTimeout: 10000,
   verbose: true,
   collectCoverage: false,
   forceExit: true,
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  globals: {
+    'ts-jest': {
+      useESM: false,
+    },
+  },
 };
